4c1fe2a8b02806b3be2db39b6d8e983a215c59f9
JackWorld <jackwong0326@gmail.com>
2022-08-18 21:24:21 +0800

    Change theme ( dark / ligth mode ) with button.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai'
+import { FiSun, FiMoon } from 'react-icons/fi'
 import { Cart } from './'
 import { useStateContext } from '../context/StateContext'
+import { useTheme } from 'next-themes'
 
 const Navbar = () => {
     const { showCart, setShowCart, totalQuantities } = useStateContext()
+    const { theme, setTheme } = useTheme()
 
     return (
         <div className="navbar-container">
             <p className="logo">
                 <Link href="/">Hello World Headphones</Link>
             </p>
-
-            <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-                <AiOutlineShopping />
-                <span className="cart-item-qty">{totalQuantities}</span>
-            </button>
-
-            {showCart && <Cart />}
+            <div className="navbar-icon">
+                <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+                    <AiOutlineShopping />
+                    <span className="cart-item-qty">{totalQuantities}</span>
+                </button>
+                {theme === 'light' ? (<button type="button" className="theme-icon" onClick={() => setTheme('dark')}><FiMoon /></button>) : (<button type="button" className="theme-icon" onClick={() => setTheme('light')}><FiSun /></button>)}
+                {showCart && <Cart />}
+            </div>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import '../styles/globals.css'
 import { Layout } from '../components';
-import { StateContext } from '../context/StateContext';
-import { Toaster } from 'react-hot-toast';
+import { StateContext } from '../context/StateContext'
+import { Toaster } from 'react-hot-toast'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
+    <ThemeProvider>
+      <StateContext>
+        <Layout>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </StateContext>
+    </ThemeProvider>
   )
 }
 
