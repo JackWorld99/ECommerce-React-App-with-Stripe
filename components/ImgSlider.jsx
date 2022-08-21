589ae7011d6464474be0986a3b6f3a5235910d76
JackWorld <jackwong0326@gmail.com>
2022-08-21 15:59:20 +0800

    Update UI style and layout.

--- components/ImgSlider.jsx
+++ components/ImgSlider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { runFireworks } from '../lib/utils'
 
 const ImgSlider = () => {
     const image = ["banner_1.png", "banner_2.png", "banner_3.png", "banner_4.png", "banner_5.png"]
@@ -23,7 +24,7 @@ const ImgSlider = () => {
             <div className="slideshow">
                 <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
                     {image.map((img, index) => (
-                        <img className="slide" key={index} src={"/img/" + img} />
+                        <img className="slide" key={index} src={"/img/" + img} onClick={() => runFireworks()} />
                     ))}
                 </div>
 
